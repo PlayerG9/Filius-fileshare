8f58397fc185238ccbbfb14e1d04eb94d6d945af
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-10-21 10:52:49 +0200

    added function to open the fuse-dir in default explorer

--- src/filius/utility/paths.py
+++ src/filius/utility/paths.py
@@ -6,6 +6,7 @@ r"""
 import sys as _sys
 import os.path as _path
 import functools as _ft
+import subprocess as _sp
 
 
 def local(*args):
@@ -25,4 +26,17 @@ def _getLocalDir():
 
 @_ft.cache
 def getMountPoint():
-    return _path.expanduser("~/felius")
+    from .config import getConfig
+    mount = getConfig('mount', default="~/felius")
+    return _path.abspath(_path.expanduser(mount))
+
+
+def openFolderInExplorer(path: str, important: bool = False):
+    if _sys.platform == 'darwin':
+        _sp.check_call(['open', '--', path])
+    elif _sys.platform == 'linux':
+        _sp.check_call(['xdg-open', path])
+    elif _sys.platform == 'win32':
+        _sp.check_call(['explorer', path])
+    elif important:
+        raise OSError('os is not supported')
